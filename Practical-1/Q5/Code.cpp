fa462879102994eba77261087d3cffdca742e0b8
Saksham Dobriyal <81918552+Saksham-88003@users.noreply.github.com>
2021-05-30 21:26:56 +0530

    Create Code.cpp

---
+++ Practical-1/Q5/Code.cpp
@@ -0,0 +1,18 @@
+//Q5. Given a full n-arry tree with I internal vertices, WAP to find number of
+//    leaf nodes.
+
+#include <bits/stdc++.h>
+using namespace std;
+// Function to calculate
+// leaf nodes in n-ary tree
+int calcNodes(int N, int I) {
+ int result = 0;
+ result = I * (N - 1) + 1;
+ return result;
+}
+// Driver code
+int main() {
+ int N = 5, I = 2;
+ cout << "Leaf nodes = " << calcNodes(N, I);
+ return 0;
+}
